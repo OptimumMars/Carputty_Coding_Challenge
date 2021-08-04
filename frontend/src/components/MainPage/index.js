1e22bcec77c78fe39d68160aa22855e9d7de012f
Gabriel <gabriel.gutz@live.com>
2021-08-04 19:54:44 -0400

    Added bare main component, cleared out some unnecessary skeleton stuff

---
+++ frontend/src/components/MainPage/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './MainPage.css';
+
+function MainPage() {
+    return (
+        <div>this is a div on the main page</div>
+    )
+};
+
+export default MainPage
